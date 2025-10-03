0e2e8ff4c509e6cffbd086f30e471f98d2a48cd6
Isaiah Gardeen <igardeen@meditech.com>
2025-10-03 16:00:41 -0500

    I will never commit a key ever again

--- src/askGemini.ts
+++ src/askGemini.ts
@@ -1,11 +1,8 @@
-import { GEMINI_API_KEY } from "../apiKey.ts";
+import { GEMINI_API_KEY } from './key.ts';
 import { randomPeriods } from "./terminalFormatting.ts";
 
 const maxTries = 5;
-export async function askGeminiWithRetry(
-  query: string,
-  allowPrintOutput = true,
-): Promise<string> {
+export async function askGeminiWithRetry(query: string, allowPrintOutput = true): Promise<string> {
   const startTime = Date.now();
   const loadingBarLength = 50;
   let num503s = 0;
@@ -52,6 +49,64 @@ export async function askGeminiWithRetry(
 }
 
 export async function askGemini(query: string): Promise<string> {
+  const API_KEY = GEMINI_API_KEY;
+  const API_URL =
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+
+  if (!API_KEY) {
+    throw new Error("GEMINI_API_KEY environment variable is not set.");
+  }
+
+  const response = await fetch(`${API_URL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-goog-api-key": API_KEY,
+    },
+    body: JSON.stringify({
+      system_instruction: {
+        parts: [{
+          text: `
+                    You're Role: You are being used in a cli tool for simple queries a developer may have when in the terminal, for this reason do not give overly long answers. 
+                    Use judgment, If the user is asking for simple commands give them the command with maybe one line of explanation
+                    Some answers may need a long answer and explanation and that is fine.
+                    `,
+        }],
+      },
+      contents: [{
+        parts: [{ text: query }],
+      }],
+      generationConfig: {
+        thinkingConfig: {
+          thinkingBudget: 0,
+        },
+      },
+    }),
+  });
+
+  if (!response.ok) {
+    const errorBody = await response.json();
+    if (response.status === 503) {
+      throw new Gemini503Error(
+        `Gemini API error: ${response.status} - ${JSON.stringify(errorBody)}`,
+      );
+    }
+    throw new Error(
+      `Gemini API error: ${response.status} - ${JSON.stringify(errorBody)}`,
+    );
+  }
+
+  const data = await response.json();
+  const candidate = data.candidates?.[0]?.content?.parts?.[0]?.text;
+
+  if (typeof candidate === "string") {
+    return candidate;
+  } else {
+    throw new Error("No valid response from Gemini API.");
+  }
+}
+
+export async function askGeminiImage(query: string): Promise<string> {
   const API_KEY = GEMINI_API_KEY;
   const API_URL =
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
---
+++ src/key.ts
@@ -0,0 +1,2 @@
+// ** DO NOT COMMIT THIS **
+export const GEMINI_API_KEY = 'gemini key goes here';
